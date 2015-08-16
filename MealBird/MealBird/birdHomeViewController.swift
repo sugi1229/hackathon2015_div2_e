5746f071c1dd7ca150c9d5d54e482f3ca425f7f4
minami <s1311389@coins.tsukuba.ac.jp>
2015-08-16 17:34:08 +0900

    写真を撮って表示させることができるようになった

--- MealBird/MealBird/birdHomeViewController.swift
+++ MealBird/MealBird/birdHomeViewController.swift
@@ -7,29 +7,133 @@
 //
 
 import UIKit
+import AVFoundation
 
 class birdHomeViewController: UIViewController {
 
+    var cameraSession : AVCaptureSession!
+    var myDevice : AVCaptureDevice!
+    var imageOutput : AVCaptureStillImageOutput!
+    var imagePreviewLayer : AVCaptureVideoPreviewLayer!
+    var takePhotoButton : UIButton!
+    var retakePhotoButton : UIButton!
+    var decidePhotoButton : UIButton!
+    
+    @IBOutlet weak var photoPreviewImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        self.setupCammera()
+        self.setupTakePhotoButtons()
+        self.hiddenPhotoPreview()
+        self.showCameraView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func setupCammera() {
+        cameraSession = AVCaptureSession()
+        
+        //デバイス取得
+        let devices = AVCaptureDevice.devices()
+        for device in devices {
+            if device.position == AVCaptureDevicePosition.Back {
+                if let d = device as? AVCaptureDevice {
+                    myDevice = d
+                }
+            }
+        }
+        
+        //画像インプット・アウトプット設定
+        if let deviceInput = AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as? AVCaptureDeviceInput {
+             cameraSession.addInput(deviceInput)
+        }
+        imageOutput = AVCaptureStillImageOutput()
+        cameraSession.addOutput(imageOutput)
+        
+        //画像表示レイヤー設定
+        imagePreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(cameraSession) as! AVCaptureVideoPreviewLayer
+        imagePreviewLayer.frame = self.view.bounds
+        imagePreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+    }
+
+    func setupTakePhotoButtons() {
+        let tabbarHeight = self.tabBarController?.tabBar.frame.size.height;
+        
+        takePhotoButton = UIButton(frame: CGRectMake(0,0,120,50))
+        takePhotoButton.backgroundColor = UIColor.redColor();
+        takePhotoButton.layer.masksToBounds = true
+        takePhotoButton.setTitle("撮影", forState: .Normal)
+        takePhotoButton.layer.cornerRadius = 20.0
+        takePhotoButton.layer.position = CGPoint(x: self.view.bounds.width/2,
+                                        y: self.view.bounds.height - takePhotoButton.frame.size.height - tabbarHeight!)
+        takePhotoButton.addTarget(self, action: "didPushTakePhotoButton:", forControlEvents: .TouchUpInside)
+        
+        retakePhotoButton = UIButton(frame: CGRectMake(0,0,120,50))
+        retakePhotoButton.backgroundColor = UIColor.blueColor();
+        retakePhotoButton.layer.masksToBounds = true
+        retakePhotoButton.setTitle("やり直す", forState: .Normal)
+        retakePhotoButton.layer.cornerRadius = 20.0
+        retakePhotoButton.layer.position = CGPoint(x: retakePhotoButton.frame.size.width/2+15,
+                                        y: self.view.bounds.height - takePhotoButton.frame.size.height - tabbarHeight!)
+        retakePhotoButton.addTarget(self, action: "didPushRetakePhotoButton:", forControlEvents: .TouchUpInside)
+        
+        decidePhotoButton = UIButton(frame: CGRectMake(0,0,120,50))
+        decidePhotoButton.backgroundColor = UIColor.greenColor();
+        decidePhotoButton.layer.masksToBounds = true
+        decidePhotoButton.setTitle("決定", forState: .Normal)
+        decidePhotoButton.layer.cornerRadius = 20.0
+        decidePhotoButton.layer.position = CGPoint(x: self.view.frame.size.width - decidePhotoButton.frame.size.width/2-15,
+                                        y: self.view.bounds.height - takePhotoButton.frame.size.height - tabbarHeight!)
+        decidePhotoButton.addTarget(self, action: "didPushDesidePhotoButton:", forControlEvents: .TouchUpInside)
+    }
+
+    func didPushTakePhotoButton(sender: AnyObject) {
+        
+        let cameraConnection = imageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        self.imageOutput.captureStillImageAsynchronouslyFromConnection(cameraConnection, completionHandler: { (imageDataBuffer, error) -> Void in
+            
+            let imageJpgData : NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+            let image : UIImage = UIImage(data: imageJpgData)!
+            self.photoPreviewImageView.image = image;
+            
+            self.hiddenCameraView()
+            self.showPhotoPreview()
+        })
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showCameraView() {
+        self.view.layer.addSublayer(imagePreviewLayer)
+        cameraSession.startRunning()
+        self.view.addSubview(takePhotoButton)
     }
-    */
-
+    
+    func hiddenCameraView() {
+         cameraSession.stopRunning()
+         imagePreviewLayer.removeFromSuperlayer()
+         takePhotoButton.removeFromSuperview()
+    }
+    
+    func showPhotoPreview() {
+        self.photoPreviewImageView.hidden = false;
+        self.view.addSubview(retakePhotoButton)
+        self.view.addSubview(decidePhotoButton)
+    }
+    
+    func hiddenPhotoPreview() {
+        photoPreviewImageView.hidden = true;
+        photoPreviewImageView.image = nil;
+        retakePhotoButton.removeFromSuperview()
+        decidePhotoButton.removeFromSuperview()
+    }
+    
+    func didPushRetakePhotoButton(sender: AnyObject) {
+        hiddenPhotoPreview()
+        showCameraView()
+    }
+    
+    func didPushDesidePhotoButton(sender: AnyObject) {
+        
+    }
+    
 }
