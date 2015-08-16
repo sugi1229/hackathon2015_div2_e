fd0d0d38c4d68b47b8c6d75207d3ae8d58913168
minami <s1311389@coins.tsukuba.ac.jp>
2015-08-16 15:53:11 +0900

    add new VC

---
+++ MealBird/MealBird/birdHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  birdHomeViewController.swift
+//  MealBird
+//
+//  Created by minami on 8/16/15.
+//  Copyright (c) 2015 div2_e. All rights reserved.
+//
+
+import UIKit
+
+class birdHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
