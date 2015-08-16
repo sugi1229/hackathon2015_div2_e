753f96092ad099112ae5612268bbb9ca5eac5b5e
minami <s1311389@coins.tsukuba.ac.jp>
2015-08-16 19:30:06 +0900

    add coredata

---
+++ MealBird/MealBird/MealBird-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MealBird-Bridging-Header.h
+//  MealBird
+//
+//  Created by minami on 8/16/15.
+//  Copyright (c) 2015 div2_e. All rights reserved.
+//
+
+#ifndef MealBird_MealBird_Bridging_Header_h
+#define MealBird_MealBird_Bridging_Header_h
+
+#import "MagicalRecord.h"
+
+#endif
---
+++ MealBird/Photo.m
@@ -0,0 +1,17 @@
+//
+//  Photo.m
+//  
+//
+//  Created by minami on 8/16/15.
+//
+//
+
+#import "Photo.h"
+
+
+@implementation Photo
+
+@dynamic image;
+@dynamic date;
+
+@end
